2ba176d013baf73aecb24a1246913fa00865e85f
nnuwansa <159515255+nnuwansa@users.noreply.github.com>
2026-01-13 22:22:40 +0530

    Update CustomUserDetailsService.java

--- Leave_backend/LeaveManagement/src/main/java/com/LeaveDataManagementSystem/LeaveManagement/Service/CustomUserDetailsService.java
+++ Leave_backend/LeaveManagement/src/main/java/com/LeaveDataManagementSystem/LeaveManagement/Service/CustomUserDetailsService.java
@@ -1,15 +1,16 @@
 package com.LeaveDataManagementSystem.LeaveManagement.Service;
 
 import com.LeaveDataManagementSystem.LeaveManagement.Repository.UserRepository;
-import com.LeaveDataManagementSystem.LeaveManagement.Model.User; // <-- your entity
+import com.LeaveDataManagementSystem.LeaveManagement.Model.User; 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
+
+
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -17,23 +18,9 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepository usersRepo;
 
-    private final String ADMIN_EMAIL = "admin@example.com";
-    private final String ADMIN_PASSWORD = "{noop}admin123";
-
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-
-        if (ADMIN_EMAIL.equals(username)) {
-            return new org.springframework.security.core.userdetails.User(
-                    ADMIN_EMAIL,
-                    ADMIN_PASSWORD,
-                    List.of(new SimpleGrantedAuthority("ROLE_ADMIN"))
-            );
-        }
-
-        // ✅ Otherwise load Employee from DB
         User user = usersRepo.findById(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
 
@@ -43,7 +30,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
         return new org.springframework.security.core.userdetails.User(
                 user.getEmail(),
-                user.getPassword(),
+                user.getPassword(),   
                 authorities
         );
     }
